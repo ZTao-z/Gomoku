f4fb6f183c616661bb5c4b8ef4a7734021a4a4a7
ZhuTao Huang <1124693098@qq.com>
2018-02-21 15:23:36 +0800

    优化了AIcontroller.js
    将warpboard的次数变为1，在最开始的地方执行

--- js/AIController.js
+++ js/AIController.js
@@ -6,8 +6,9 @@ generateAllNextPossibleMove(wrapBoard(matrix), color) //落子位置函数
 
 */
 
-const MIN = -4294967295;
-const MAX = 4294967295;
+const MIN = Number.NEGATIVE_INFINITY;
+const MAX = Number.POSITIVE_INFINITY;
+
 var alpha = Number.NEGATIVE_INFINITY,
 	beta = Number.POSITIVE_INFINITY;
 
@@ -15,13 +16,22 @@ var alpha = Number.NEGATIVE_INFINITY,
 var FunctionMaxMin = function(board, color, deep) {
 	var best = MIN;
 	//console.log(board);
-	var points = StepGenerator.generateAllNextPossibleMove(StepGenerator.copyAndWrapBoard(board), color);
+	var boardTemp = StepGenerator.copyAndWrapBoard(board);
+	var points = StepGenerator.generateAllNextPossibleMove(boardTemp, color);
 	var bestPoints = [];
 	//console.log(points);
 	for(var i = 0; i < points.length; i++){
 		var now_point = points[i];
-		board[now_point[0]][now_point[1]] = color;         //下子的颜色
-		var value = min(board, Math.abs(color-1), alpha, beta, deep-1);
+		boardTemp[now_point[0]+2][now_point[1]+2] = color;         //下子的颜色
+
+		var value = min(boardTemp, Math.abs(color-1), alpha, beta, deep-1);
+
+		if(value > best)
+			best = value;
+		if(value > alpha)
+			alpha = best;
+		if(beta <= alpha)
+			break;
 
 		if(value == best){
 			bestPoints.push(now_point);
@@ -34,7 +44,7 @@ var FunctionMaxMin = function(board, color, deep) {
 		else{
 			console.log('ignore');
 		}
-		board[now_point[0]][now_point[1]] = 'e'; 
+		boardTemp[now_point[0]+2][now_point[1]+2] = 'e'; 
 	}
 	console.log(bestPoints.length);
 	var result = bestPoints[Math.floor(Math.random() * bestPoints.length)];
@@ -50,13 +60,13 @@ var max = function(board, color, alpha, beta, deep){
 		return v;
 
 	var best = MIN;
-	var points = StepGenerator.generateAllNextPossibleMove(StepGenerator.copyAndWrapBoard(board), color);
+	var points = StepGenerator.generateAllNextPossibleMove(board, color);
 
 	for( var i = 0; i < points.length; i++){
 		var p = points[i];
-		board[p[0]][p[1]] = color;
+		board[p[0]+2][p[1]+2] = color;
 		var v = min(board, Math.abs(color-1), alpha, beta, deep-1);
-		board[p[0]][p[1]] = 'e';
+		board[p[0]+2][p[1]+2] = 'e';
 		if(v > best)
 			best = v;
 		if(v > alpha)
@@ -75,13 +85,13 @@ var min = function(board, color, alpha, beta, deep){
 		return v;
 
 	var best = MAX;
-	var points = StepGenerator.generateAllNextPossibleMove(StepGenerator.copyAndWrapBoard(board), color);
+	var points = StepGenerator.generateAllNextPossibleMove(board, color);
 
 	for( var i = 0; i < points.length; i++){
 		var p = points[i];
-		board[p[0]][p[1]] = color;
+		board[p[0]+2][p[1]+2] = color;
 		var v = max(board, Math.abs(color-1), alpha, beta, deep-1);
-		board[p[0]][p[1]] = 'e';
+		board[p[0]+2][p[1]+2] = 'e';
 		if(v < best)
 			best = v;
 		if(v < beta)
